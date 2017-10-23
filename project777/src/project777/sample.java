e159f796d819ba7128103ae4299dc4b69a9decfd
lee <xoutlaw@naver.com>
2017-10-23 15:59:35 +0900

    sssssssssssss

--- project777/src/project777/sample.java
+++ project777/src/project777/sample.java
@@ -5,6 +5,8 @@ public class sample {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("헬로월드");
+		System.out.println("HelloWorld");
+		System.out.println("헬로월드");
 	}
 
 }
